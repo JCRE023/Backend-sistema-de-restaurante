39187903b290ed917a10d4e0a6bffc0184aef864
JohanP31 <johanpbermudez@hotmail.com>
2026-02-22 20:24:23 -0500

    feat: creacion de la entidad mesa

--- src/entities/Mesa.py
+++ src/entities/Mesa.py
@@ -1,39 +1,30 @@
 class Mesa:
-    def __init__(self, nombre, sillas, disponible):
+    """
+    Crea un objeto mesa para el restaurante
+    """
+
+    def __init__(self, nombre: str, sillas: int, disponible: bool):
         self.nombre = nombre
         self.sillas = sillas
         self.disponible = disponible
 
 
-lista_de_mesas = []
-
-mesa_1 = Mesa("Mesa 1", 4, True)
-lista_de_mesas.append(mesa_1)
-
-mesa_2 = Mesa("Mesa 2", 6, True)
-lista_de_mesas.append(mesa_2)
-
-mesa_3 = Mesa("Mesa 3", 4, True)
-lista_de_mesas.append(mesa_3)
-
-mesa_4 = Mesa("Mesa 4", 4, True)
-lista_de_mesas.append(mesa_4)
-
-mesa_5 = Mesa("Mesa 5", 2, True)
-lista_de_mesas.append(mesa_5)
-
-mesa_6 = Mesa("Mesa 6", 8, True)
-lista_de_mesas.append(mesa_6)
-
-mesa_7 = Mesa("Mesa 7", 8, True)
-lista_de_mesas.append(mesa_7)
-
-mesa_8 = Mesa("Mesa 8", 2, True)
-lista_de_mesas.append(mesa_8)
+lista_de_mesas = [
+    Mesa("Mesa 1", 4, True),
+    Mesa("Mesa 2", 6, True),
+    Mesa("Mesa 3", 4, True),
+    Mesa("Mesa 4", 4, True),
+    Mesa("Mesa 5", 2, True),
+    Mesa("Mesa 6", 8, True),
+    Mesa("Mesa 7", 8, True),
+    Mesa("Mesa 8", 2, True),
+]
 
 
 def Mostrar_Mesas_Disponibles():
-
+    """
+    Imprime con un SI o NO si la mesa esta ocupada
+    """
     print("Mesas totales:")
     for mesa in lista_de_mesas:
         if mesa.disponible is True:
@@ -43,17 +34,17 @@ def Mostrar_Mesas_Disponibles():
 
 
 def actualizar_disponibilidad():
+    """
+    Actualiza a las mesas, si estan disponibles o ocupadas
+    """
     buscar = input("Introduce el nombre de la mesa (Ej: Mesa 3): ")
-
     for mesa in lista_de_mesas:
         if mesa.nombre == buscar:
             respuesta = input(f"¿Está la {mesa.nombre} disponible? (si/no): ").lower()
-
             if respuesta == "si":
                 mesa.disponible = True
             else:
                 mesa.disponible = False
-
             print(f"La disponibilidad de la {mesa.nombre} ha sido actualizada.")
             break
 
