8d94bbd01776909b35419f3489e82554efb46963
JohanP31 <johanpbermudez@hotmail.com>
2026-03-14 20:48:50 -0500

    feat: creacion de entidad mesa

---
+++ src/entities/Mesa.py
@@ -0,0 +1,32 @@
+import uuid
+
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import Column, String, Float, ForeignKey, DateTime, Text
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+from src.database.config import Base
+
+
+class Mesa(Base):
+
+    __tablename__ = "mesas"
+
+    id_mesa = Column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True
+    )
+    numero_mesa = Column(String(50), nullable=False, unique=True)
+    estado = Column(String(20), nullable=False, default="disponible")
+
+    fecha_creacion = Column(DateTime(timezone=True), server_default=func.now())
+    fecha_edicion = Column(DateTime(timezone=True), onupdate=func.now())
+
+    id_usuario_creacion = Column(
+        UUID(as_uuid=True), ForeignKey("usuarios.id_usuario"), nullable=False
+    )
+    id_usuario_edicion = Column(
+        UUID(as_uuid=True), ForeignKey("usuarios.id_usuario"), nullable=True
+    )
+
+    usuario_creacion = relationship("Usuario", foreign_keys=[id_usuario_creacion])
+    usuario_edicion = relationship("Usuario", foreign_keys=[id_usuario_edicion])
