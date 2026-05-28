d9692a75f331316f593a3451c6ff88684c51d6d2
Juan <camiilo1023@hotmail.com>
2026-05-27 19:50:49 -0500

    fix: add production Vercel domain to CORS

--- src/api/app.py
+++ src/api/app.py
@@ -38,6 +38,7 @@ app.add_middleware(
         "http://localhost:4200",
         "http://127.0.0.1:4200",
         "https://frontend-sistema-de-restaurante-9287wjv10.vercel.app",
+        "https://frontend-sistema-de-restaurante.vercel.app",
     ],
     allow_methods=["*"],
     allow_headers=["*"],
